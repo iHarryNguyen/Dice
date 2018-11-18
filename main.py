e7000e074156a8c5cfc5b0987b8542ee42eeb8b8
Harry Nguyen <hnguyen@asmarterwindow.com>
2018-11-18 14:16:27 -0500

    Added loops and simplified functions

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ play1 = input("Player 1 name?")
 play2 = input("Player 2 name?")
 prize = input("What does the loser have to do?")
 
-print("Hi " + play1 + " & " + play2 + ", let's roll the dice of destiny.")
+print("Hi " + play1 + " & " + play2 + ", let's roll the dice.")
 
 play1Num = input(play1 + " choose a number 1-6.")
 play2Num = input(play2 + " choose a number 1-6.")
@@ -20,16 +20,22 @@ play2Num = input(play2 + " choose a number 1-6.")
 play1Num = int(play1Num)
 play2Num = int(play2Num)
 
-if play1Num == diceNumber:
+def rollDice():asdr
     print("The Dice rolled...")
     print(diceNumber)
-    print(play1.upper() + " WINS!")
-    print(play2 + " must: " + prize)
-    sys.exit()
 
-if play2Num == diceNumber:
-    print("The Dice rolled...")
-    print(diceNumber)
-    print(play2.upper() + " WINS!")
-    print(play1 + " must: " + prize)
-    sys.exit()
+while diceNumber != play1Num or play2Num:
+    diceNumber = random.randint(1,6)
+
+
+    if play1Num == diceNumber:
+        rollDice()
+        print(play1.upper() + " WINS!")
+        print(play2 + " must: " + prize)
+        sys.exit()
+
+    if play2Num == diceNumber:
+        rollDice()
+        print(play2.upper() + " WINS!")
+        print(play1 + " must: " + prize)
+        sys.exit()
